27dc9c1da69bbeec5bb92a7f7cb1ffea87051bbd
Ashish <sbhargava.ashish@gmail.com>
2017-12-17 12:23:30 +0530

    Comments added

--- Builder/src/TextBuilderTest.java
+++ Builder/src/TextBuilderTest.java
@@ -8,6 +8,8 @@ class TextBuilder{
 	}
 	
 	public TextBuilder append(String text) {
+		// This is Just for Example Do not do like that as its not a good Practice
+		// It will result in Extra Unnecessary String object creatino
 		this.text = this.text + text ;
 		return this;
 	}
